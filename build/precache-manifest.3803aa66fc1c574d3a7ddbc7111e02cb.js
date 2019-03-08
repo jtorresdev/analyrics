8f4410646336043b85cbc02a901096e1f907c26f
Jesus Torres <jesusk01@gmail.com>
2019-03-08 12:03:03 -0500

    build

---
+++ build/precache-manifest.3803aa66fc1c574d3a7ddbc7111e02cb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "94e29a83b996a3c9d49a",
+    "url": "/static/css/main.c2783e8d.chunk.css"
+  },
+  {
+    "revision": "94e29a83b996a3c9d49a",
+    "url": "/static/js/main.94e29a83.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "26b19f4ac360d3399cb7",
+    "url": "/static/js/2.26b19f4a.chunk.js"
+  },
+  {
+    "revision": "0c0ac660d489007641b7d735f017c863",
+    "url": "/index.html"
+  }
+];
