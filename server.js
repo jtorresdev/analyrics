50064d4865ab43b8784540fdad2c35685d4be9e1
Jesus Torres <jesusk01@gmail.com>
2019-03-08 11:50:40 -0500

    serv

--- server.js
+++ server.js
@@ -1,35 +1,28 @@
 var express = require('express');
-var cors = require('cors')
+var cors = require('cors');
 var app = express();
 const path = require('path');
 const spawn = require('child_process').spawn;
 const port = process.env.PORT || 5000;
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
-app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, 'client/build')));
 
-app.get('*', function (req, res) {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+app.post('/getlyrics', function(req, res) {
+	const name = req.body.name;
+	const artist = req.body.artist;
+	const pythonProcess = spawn('python', [ 'genius.py', name, artist ]);
+	pythonProcess.stdout.on('data', (data) => {
+		res.send(data);
+	});
 });
 
-app.get('/algo', function(req, res){
-    res.send('algo')
-})
-
-app.post('/getlyrics', function (req, res) {
-    const name = req.body.name
-    const artist = req.body.artist
-    const pythonProcess = spawn('python', ["genius.py", name, artist]);
-    pythonProcess.stdout.on('data', (data) => {
-        res.send(data);
-    });
-    
+app.get('*', (req, res) => {
+	res.sendFile(path.join(__dirname + '/build/index.html'));
 });
 
-
-app.listen(port, function () {
-  console.log('Example app listening on port' + port);
+app.listen(port, function() {
+	console.log('Example app listening on port' + port);
 });
