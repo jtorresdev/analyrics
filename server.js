077430e2a41ebca0a33f3bd32a5dfa3c2c23d8fc
Jesus Torres <jesusk01@gmail.com>
2019-03-08 11:38:54 -0500

    server

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var cors = require('cors')
 var app = express();
 const path = require('path');
 const spawn = require('child_process').spawn;
+const port = process.env.PORT || 5000;
 
 app.use(cors())
 app.use(express.json())
@@ -28,6 +29,7 @@ app.post('/getlyrics', function (req, res) {
     
 });
 
-app.listen(4000, function () {
-  console.log('Example app listening on port 4000!');
+
+app.listen(port, function () {
+  console.log('Example app listening on port' + port);
 });
